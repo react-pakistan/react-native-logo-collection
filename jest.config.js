bb3eb9982ccc7e374b531532e44fd5e2ec6f4736
Taimoor Khan <taimoor.muhammad@arbisoft.com>
2021-02-13 15:13:55 +0500

    :recycle: minor changes

--- jest.config.js
+++ jest.config.js
@@ -3,4 +3,14 @@ module.exports = {
   rootDir: './src',
   testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.(tsx?|ts?)$',
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  setupFiles: [
+    '<rootDir>/__tests__/test-env.js',
+  ],
+  coverageReporters: ["json-summary", "lcov", "text"],
+  coverageDirectory: "<rootDir>/../coverage",
+  collectCoverageFrom: [
+    "<rootDir>/**/*.{ts,tsx,js,jsx}",
+    "!<rootDir>/**/*.story.{ts,tsx,js,jsx}",
+    "!<rootDir>/xml/"
+  ]
 };
